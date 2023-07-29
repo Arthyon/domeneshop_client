fe91db031854ca19ec100aa7b1f43ad0bac18968
Christian Hochlin <Arthyon@users.noreply.github.com>
2023-07-29 09:04:29 +0200

    Add better documentation to MockClient and HttpClient

--- src/http.rs
+++ src/http.rs
@@ -6,6 +6,26 @@ use crate::errors::DomeneshopError;
 #[async_trait]
 /// A trait used to abstract the transport mechanism for sending requests.
 /// This is mostly added to make testing easier.
+///
+/// If one wants to use another crate than `reqwest`, a separate implementation of this trait can be supplied when creating the [`DomeneshopClient`](./client/struct.DomeneshopClient.html).
+///
+/// # Example
+/// To implement this trait, you need the [`async_trait`](https://docs.rs/async-trait/latest/async_trait/)-crate.
+/// ```
+///use async_trait::async_trait;
+///use http_types::{Request, Response};
+///use domeneshop_client::{errors::DomeneshopError, http::HttpClient};
+///
+///struct MyClient;
+///#[async_trait]
+///impl HttpClient for MyClient
+///{
+///    async fn execute_request(&self, request: Request) -> Result<Response, DomeneshopError> {
+///        todo!();
+///    }
+///}
+///
+/// ```
 pub trait HttpClient {
     /// Executes a request
     /// This defaults to a provided `reqwest`-implementation, but custom implementations can be created
--- src/http_client/mock.rs
+++ src/http_client/mock.rs
@@ -2,8 +2,46 @@ use async_trait::async_trait;
 use http_types::{Request, Response};
 
 use crate::{errors::DomeneshopError, http::HttpClient};
-
-/// A mock client that can be used for testing code using this crate
+/// A mock client that can be used for testing code using this crate.
+/// NOTE: This is only available when feature `mock` is enabled.
+///
+/// # Example
+/// ```
+/// use domeneshop_client::client::{ DomeneshopClientConfiguration, DomeneshopClient };
+/// use domeneshop_client::http_client::mock::MockClient;
+/// use domeneshop_client::errors::DomeneshopError;
+/// use http_types::{ StatusCode, Response, Request };
+///
+/// # use std::error::Error;
+/// # fn main() -> Result<(), Box<dyn Error>> {
+/// # tokio_test::block_on(async {
+/// async fn receive_request(req: Request) -> Result<Response, DomeneshopError> {
+///     let mut response = Response::new(StatusCode::Ok);
+///     response.set_body("[]");
+///     Ok(response)
+/// }
+///
+/// let mock = MockClient {
+///     req_received: receive_request,
+/// };
+///
+/// let client = DomeneshopClient::new(
+///     String::from("token"),
+///     String::from("secret"),
+///     DomeneshopClientConfiguration {
+///         user_agent: None,
+///         base_url: Some("https://localhost".to_string()),
+///         underlying_client: Some(Box::new(mock)),
+///     }
+/// )?;
+/// let domains = client.list_domains_with_filter(".no").await?;
+///
+/// assert_eq!(domains.len(), 0);
+/// #
+/// #     Ok(())
+/// # })
+/// # }
+/// ```
 pub struct MockClient<F>
 where
     F: std::future::Future<Output = Result<Response, DomeneshopError>> + Send,
