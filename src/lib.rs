ccacd0227cb9b05ca6c2123be2320c341dd5f42a
Christian Hochlin <Arthyon@users.noreply.github.com>
2023-07-29 18:21:00 +0200

    Revert changes to lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,7 @@
 //! # domeneshop_client
 //! API bindings for the [Domeneshop API](https://api.domeneshop.no/docs).
 //!
+//!
 //! ## Prerequisites
 //! To use this crate, you need to generate API-credentials for the Domeneshop API.
 //! See their [Authentication](https://api.domeneshop.no/docs/#section/Authentication)-section for more information.
